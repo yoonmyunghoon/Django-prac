7c1f302968d3a9407bdc481541abe3fa71233421
yoonmyunghoon <youn1791472@gmail.com>
2021-02-18 17:41:40 +0900

    require_POST 데코레이터 적용

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import ValidationError
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
+from django.views.decorators.http import require_POST
 
 
 def index(request):
@@ -41,13 +42,16 @@ def detail(request, article_pk):
     return render(request, "articles/detail.html", context)
 
 
+@require_POST
 def delete(request, article_pk):
     # article = Article.objects.get(pk=article_pk)
     article = get_object_or_404(Article, pk=article_pk)
-    if request.method == "POST":
-        article.delete()
-        return redirect("articles:index")
-    return redirect("articles:detail", article.pk)
+    article.delete()
+    return redirect("articles:index")
+    # if request.method == "POST":
+    #     article.delete()
+    #     return redirect("articles:index")
+    # return redirect("articles:detail", article.pk)
 
 
 def update(request, article_pk):
@@ -69,26 +73,37 @@ def update(request, article_pk):
     return render(request, "articles/form.html", context)
 
 
+@require_POST
 def comments_create(request, article_pk):
     # article = Article.objects.get(pk=article_pk)
     # article = get_object_or_404(Article, pk=article_pk)
-    if request.method == "POST":
-        # content = request.POST.get("content")
-        # comment = Comment(article=article, content=content)
-        # comment.save()
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.article_id = article_pk
-            comment.save()
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article_id = article_pk
+        comment.save()
     return redirect("articles:detail", article_pk)
+    # if request.method == "POST":
+    #     # content = request.POST.get("content")
+    #     # comment = Comment(article=article, content=content)
+    #     # comment.save()
+    #     comment_form = CommentForm(request.POST)
+    #     if comment_form.is_valid():
+    #         comment = comment_form.save(commit=False)
+    #         comment.article_id = article_pk
+    #         comment.save()
+    # return redirect("articles:detail", article_pk)
 
 
+@require_POST
 def comments_delete(request, article_pk, comment_pk):
-    if request.method == "POST":
-        comment = get_object_or_404(Comment, pk=comment_pk)
-        comment.delete()
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    comment.delete()
     return redirect("articles:detail", article_pk)
+    # if request.method == "POST":
+    #     comment = get_object_or_404(Comment, pk=comment_pk)
+    #     comment.delete()
+    # return redirect("articles:detail", article_pk)
 
 
 def comments_update(request, article_pk, comment_pk):
